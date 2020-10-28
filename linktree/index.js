d79aa99a9de124ca5d340964c200150cdcc4e242
Vikram Singh <vikzilla18@gmail.com>
2020-10-27 22:11:39 -0400

    Updated page to have profile image and name

--- linktree/index.js
+++ linktree/index.js
@@ -7,6 +7,9 @@ const linksArray =
         { 'name': 'GitHub', 'url': 'https://github.com/airloaf' }
     ];
 
+const name = "Vikram Singh"
+const profileImageURL = "https://avatars2.githubusercontent.com/u/13282682?s=460&u=a5689107f5785c9a98338b7624bfe0cb8bf3c279&v=4"
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
@@ -27,7 +30,10 @@ async function generatePage(request) {
         }
     )
     const linksPage = new HTMLRewriter().on("div#links", new LinksTransformer(linksArray)).transform(response);
-    return linksPage
+    const profilePage = new HTMLRewriter().on("div#profile", new ProfileTransformer()).transform(linksPage);
+    const namePage = new HTMLRewriter().on("h1#name", new NameTransformer(name)).transform(profilePage);
+    const avatarPage = new HTMLRewriter().on("img#avatar", new AvatarTransformer(profileImageURL)).transform(namePage);
+    return avatarPage; 
 }
 
 async function handleRequest(request) {
@@ -46,5 +52,30 @@ class LinksTransformer {
         this.links.forEach(link =>{
             element.append(`<a href=\"${link.url}\">${link.name}</a>`, {html: true});
         })
+    }
+}
+
+class ProfileTransformer {
+    async element(element) {
+        // the style attribute contains "display: none"
+        element.removeAttribute('style');
+    }
+}
+
+class NameTransformer{
+    constructor(name){
+        this.name = name;
+    }
+    async element(element) {
+        element.append(this.name);
+    }
+}
+
+class AvatarTransformer{
+    constructor(imageURL){
+        this.imageURL = imageURL;
+    }
+    async element(element) {
+        element.setAttribute('src', this.imageURL);
     }
 }
