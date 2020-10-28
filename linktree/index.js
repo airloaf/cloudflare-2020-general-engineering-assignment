ad37e2ee11185bbd2c6752ee767b8e051f7304aa
Vikram Singh <vikzilla18@gmail.com>
2020-10-27 21:54:22 -0400

    Using HTMLRewriter to render links

--- linktree/index.js
+++ linktree/index.js
@@ -1,8 +1,12 @@
 const Router = require('./router')
 
-/**
- * Example of how router can be used in an application
- *  */
+const linksArray =
+    [
+        { 'name': 'Personal Website', 'url': 'https://airloaf.github.io/' },
+        { 'name': 'LinkedIn', 'url': 'https://www.linkedin.com/in/vikram-singh-ce/' },
+        { 'name': 'GitHub', 'url': 'https://github.com/airloaf' }
+    ];
+
 addEventListener('fetch', event => {
     event.respondWith(handleRequest(event.request))
 })
@@ -11,21 +15,36 @@ function links(request) {
     const init = {
         headers: { 'content-type': 'application/json' },
     }
-    const body = JSON.stringify(
-                [
-                    { 'name': 'Personal Website', 'url': 'https://airloaf.github.io/' },
-                    { 'name': 'LinkedIn', 'url': 'https://www.linkedin.com/in/vikram-singh-ce/' },
-                    { 'name': 'GitHub', 'url': 'https://github.com/airloaf' }
-                ]
-    )
+    const body = JSON.stringify(linksArray)
     return new Response(body, init)
+}
+
+async function generatePage(request) {
+    response = await fetch(
+        'https://static-links-page.signalnerve.workers.dev',
+        {
+            headers: { 'content-type': 'text/html' }
+        }
+    )
+    const linksPage = new HTMLRewriter().on("div#links", new LinksTransformer(linksArray)).transform(response);
+    return linksPage
 }
 
 async function handleRequest(request) {
     const r = new Router()
-    // Replace with the appropriate paths and handlers
     r.get('/links', request => links(request))
-
+    r.get('/.*', request => generatePage(request))
     const resp = await r.route(request)
     return resp
+}
+
+class LinksTransformer {
+    constructor(links) {
+        this.links = links;
+    }
+    async element(element) {
+        this.links.forEach(link =>{
+            element.append(`<a href=\"${link.url}\">${link.name}</a>`, {html: true});
+        })
+    }
 }
